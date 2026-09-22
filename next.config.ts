855d54edd363e3a463495a3408e97d76ef0f4ee5
BCA-445-11397-Virendra <166232903+CodeNonStopOfficial@users.noreply.github.com>
2026-09-23 01:34:38 +0530

    add hero section

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+   images : {
+     remotePatterns : [
+       {
+         protocol : "https",
+         hostname : "clinquant-faloodeh-70c1be.netlify.app"
+       }
+     ]
+   }
 };
 
 export default nextConfig;
